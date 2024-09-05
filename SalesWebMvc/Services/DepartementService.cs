34d6855c0a810c8495b00344d471be1fed6574c9
franchesco aprigio <franchescodsa@outlook.com>
2024-09-05 10:36:00 -0300

    SellerFormViewModel and Department select component

---
+++ SalesWebMvc/Services/DepartementService.cs
@@ -0,0 +1,23 @@
+﻿using SalesWebMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Services
+{
+    public class DepartementService
+    {
+        private readonly SalesWebMvcContext _context;
+
+        public DepartementService(SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+
+        public List<Department> FindAll()
+        {
+            return _context.Department.OrderBy(x => x.Name).ToList();
+        }
+    }
+}
